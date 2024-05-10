98ddef026ac71c67e8fe0c44d556239aa6135df6
P. Kevin Contreras <kevin.macwhinnie@me.com>
2024-05-10 09:43:23 -0400

    Some initial condition coloring

--- app/views/_app.tsx
+++ app/views/_app.tsx
@@ -2,31 +2,32 @@ import i18next, { t } from "i18next";
 import { ComponentChildren } from "preact";
 
 export interface AppProps {
+    readonly className?: string;
     readonly children: ComponentChildren;
 }
 
-export function App({children}: AppProps) {
+export function App({ className, children }: AppProps) {
     return (
         <html lang={i18next.resolvedLanguage}>
-        <head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="stylesheet" href="/styles/main.css" />
-            <link rel="stylesheet" href="/styles/weather-icons.min.css" />
-            <link rel="stylesheet" href="/styles/weather-icons-wind.min.css" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
-            <link href="https://fonts.googleapis.com/css2?family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-            <title>Outside</title>
-        </head>
-        <body>
-            <main>
-                <h1>{t('appName')}</h1>
-                {children}
-                <footer>{t('appCopyright')}</footer>
-            </main>
-        </body>
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" href="/styles/main.css" />
+                <link rel="stylesheet" href="/styles/weather-icons.min.css" />
+                <link rel="stylesheet" href="/styles/weather-icons-wind.min.css" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+                <link href="https://fonts.googleapis.com/css2?family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+                <title>Outside</title>
+            </head>
+            <body className={className}>
+                <main>
+                    <h1>{t('appName')}</h1>
+                    {children}
+                    <footer>{t('appCopyright')}</footer>
+                </main>
+            </body>
         </html>
     );
 }
