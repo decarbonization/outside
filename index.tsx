c8148d32bdaa95307ee1e35159b2ba45bc6a8239
P. Kevin Contreras <kevin.macwhinnie@me.com>
2024-05-21 15:44:17 -0400

    Fix up licensing headers

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 /*
  * outside weather app
- * Copyright (C) 2014  Peter "Kevin" Contreras
+ * Copyright (C) 2024  MAINTAINERS
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
