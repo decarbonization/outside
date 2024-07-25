a8fb0dbae2cd90d48ed833bf4681704badeeb1cd
P. Kevin Contreras <kevin.macwhinnie@me.com>
2024-07-25 19:33:23 -0400

    Fix broken test :eyeroll:

--- test/server/routes/weather-routes.test.ts
+++ test/server/routes/weather-routes.test.ts
@@ -5,7 +5,7 @@ describe("weather-routes module", () => {
     describe("#WeatherRoutes", () => {
         describe("#linkToGetWeather", () => {
             it("should reduce precision of geo coordinates", () => {
-                expect(WeatherRoutes.linkToGetWeather("US", { latitude: 40.7129822, longitude: -74.007205 }, "New York")).toStrictEqual("/weather/US/40.712/-74.008/New%20York");
+                expect(WeatherRoutes.linkToGetWeather({ countryCode: "US", location: { latitude: 40.7129822, longitude: -74.007205 }, query: "New York" })).toStrictEqual("/weather/US/40.712/-74.008/New%20York");
             });
         });
     });
